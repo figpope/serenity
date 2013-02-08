c3e34297f367cd2140bc59ebe3abe0548f547b3f
Andrew Pope <drewii2ii@gmail.com>
2013-02-08 13:36:14 -0500

    Began API specification

---
+++ routes/twilio.js
@@ -0,0 +1,6 @@
+var redis = require("redis-url").connect(process.env.MYREDIS_URL),
+	twilio = require('twilio');
+
+exports.findAll = function(req, res) {
+
+};
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require("express"),
+	http = require('http'),
 	routes = require("./routes/twilio");
 
 var app = express();
 
-app.get("/", function(req, res) {
-
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.listen(3000);
-console.log('Listening on port 3000...')
+app.configure('development', function () {
+	app.use(express.errorHandler());
+});
+
+app.get('/getToken', twilio.newToken(req));
+app.get('/getConference', twilio.newToken(req));
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
+});
