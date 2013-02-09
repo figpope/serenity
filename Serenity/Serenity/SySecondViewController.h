c12347fe170304eea41a75f979812d7893021687
Brian Donghee Shin <ds2791@columbia.edu>
2013-02-09 00:53:40 -0500

    Reinit iOS

--- Serenity/Serenity/SySecondViewController.h
+++ Serenity/Serenity/SySecondViewController.h
@@ -2,7 +2,7 @@
 //  SySecondViewController.h
 //  Serenity
 //
-//  Created by Brian Donghee Shin on 2/8/13.
+//  Created by Brian Donghee Shin on 2/9/13.
 //  Copyright (c) 2013 brishin. All rights reserved.
 //
 
