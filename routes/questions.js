e8022150530105de14b348d6bea93b4013a63bea
Andrew Pope <drewii2ii@gmail.com>
2013-02-09 02:52:34 -0500

    updated questions

--- routes/questions.js
+++ routes/questions.js
@@ -13,7 +13,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () { console.log("   Connection to mongo database successful!"); });
 
 exports.get = function (req, res) {
-
+	
 };
 
 exports.add = function (req, res) {
