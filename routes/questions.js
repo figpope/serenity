bb2d2a99e99869dc6cd06d3b22ea968c152d347c
Andrew Pope <drewii2ii@gmail.com>
2013-02-09 02:53:16 -0500

    Updated questions

--- routes/questions.js
+++ routes/questions.js
@@ -1,4 +1,16 @@
-var redis = require("redis-url").connect(process.env.MYREDIS_URL | "redis://localhost:6379");
+var mongoose = require('mongoose'),
+	models = require('../models');
+
+if(process.env.MONGO_URL) {
+	config.mongo.url = process.env.MONGO_URL;
+} else { 
+	config = require("../config");
+}
+
+mongoose.connect(config.mongo.url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () { console.log("   Connection to mongo database successful!"); });
 
 exports.get = function (req, res) {
 	
